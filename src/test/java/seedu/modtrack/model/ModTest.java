a407c52f298a78d42fcf2cdb3eb798e50cfcb79f
GGBondxX <175328012+GGBondxX@users.noreply.github.com>
2026-04-09 20:22:10 +0800

    fixed gradle check issues

--- src/test/java/seedu/modtrack/model/ModTest.java
+++ src/test/java/seedu/modtrack/model/ModTest.java
@@ -2,7 +2,6 @@ package seedu.modtrack.model;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
